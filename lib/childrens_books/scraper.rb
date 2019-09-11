478dddd765d9a7877fe884984b4c83de4be623d3
janewysoki <jane.wysoki@gmail.com>
2019-09-11 15:01:43 -0400

    Properly update scraper file

---
+++ lib/childrens_books/scraper.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module ChildrensBooks
+    class Scraper
+        def self.scrape
+            html = open("https://www.commonsensemedia.org/lists/50-books-all-kids-should-read-before-theyre-12")
+            doc = Nokogiri::HTML(html)
+            
+           
+
+            doc.css(".views-row-odd").each do |node|
+                title = node.css("strong.field-content a").text
+                author = node.css("div.views-field-field-term-book-authors").text.strip.delete("By").split.join(" ")
+                description = node.css("div.views-field-field-one-liner").text.strip
+                age = node.css("div.views-field-field-review-recommended-age").text.strip.delete("age").delete("+").split.join("")
+                year = node.css("div.views-field-field-canonical-date").text.strip.gsub(/[()]/, "") 
+                url = node.css("strong.field-content a").attr("href").value
+            end
+            
+            doc.css(".views-row-even").each do |node|
+                title = node.css("strong.field-content a").text
+                author = node.css("div.views-field-field-term-book-authors").text.strip.delete("By").split.join(" ")
+                description = node.css("div.views-field-field-one-liner").text.strip
+                age = node.css("div.views-field-field-review-recommended-age").text.strip.delete("age").delete("+").split.join("")
+                year = node.css("div.views-field-field-canonical-date").text.strip.gsub(/[()]/, "")
+                url = node.css("strong.field-content a").attr("href").value
+            end
+
+        end
+
+        
+    end
+
+end
+ChildrensBooks::Scraper.scrape
