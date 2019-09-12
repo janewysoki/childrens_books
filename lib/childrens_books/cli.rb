3a75741eeb2466fc4965e063c03c1f908d2735e8
janewysoki <jane.wysoki@gmail.com>
2019-09-11 20:08:12 -0400

    Add heredoc to menu

--- lib/childrens_books/cli.rb
+++ lib/childrens_books/cli.rb
@@ -1,17 +1,34 @@
-class ChildrensBooks::CLI
+module ChildrensBooks
+    class CLI
 
-    def call 
-        puts "Welcome to the Children's Books Database!"
-        menu
-        #goodbye
-    end
+        def call 
+            puts "Welcome to the Children's Books Database!"
+            menu
+            #goodbye
+        end
 
-    def menu
-        puts "Select an age range to see the best children's book suggestions!"
-        1. Ages 2-4
-        2. Ages 5-7
-        3. Ages 8-9
-        4. Ages 10-12
-        5. Surprise me!
+        def menu
+            puts <<-MENU
+            "Select an age range to see the best children's book suggestions!"
+            1. Ages 2-4
+            2. Ages 5-7
+            3. Ages 8-9
+            4. Ages 10-12
+            5. Surprise me!
+            MENU
+        end
+
+        def list_books
+            Book.all.each.with_index(1) do |book, index|
+                puts "#{index}. #{book.title}"
+            end
+        end
+
+        def list_books_by_age
+            if age == 2 || 3 || 4
+        end
+
+        def surprise_me
+        end
     end
 end
