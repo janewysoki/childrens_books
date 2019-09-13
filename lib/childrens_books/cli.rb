b8d4513510b1c1a5a624b18c0eefe49b239fbbf0
janewysoki <jane.wysoki@gmail.com>
2019-09-12 22:10:25 -0400

    Create print methods to condense call method

--- lib/childrens_books/cli.rb
+++ lib/childrens_books/cli.rb
@@ -1,34 +1,78 @@
+require 'pry'
+require_relative './book.rb'
+require_relative './scraper.rb'
+
 module ChildrensBooks
     class CLI
 
         def call 
+            ChildrensBooks::Scraper.scrape
+            puts ""
             puts "Welcome to the Children's Books Database!"
+            puts ""
+            @input = nil #ask what this does and why input is an instance variable
             menu
-            #goodbye
+            while @input != "exit"
+                @input = gets.chomp
+                if @input == "1"
+                    puts "Here are some books 2-4 year olds will love:"
+                    print_preschoolbooks
+                elsif @input == "2"
+                    puts "Here are some books 5-7 year olds will love:"
+                elsif @input == "3"
+                    puts "Here are some books 8-9 year olds will love:"
+                elsif @input == "4"
+                    puts "Here are some books 10-12 year olds will love:"
+                elsif @input == "5"
+                    puts "Here's a randomly selected book for you to try:"
+                    #ChildrensBooks::Book.surprise_me
+                elsif @input == "menu"
+                    menu
+                elsif @input == "exit"
+                    
+                else 
+                    puts "Sorry, you did not select a valid number. Please try again."
+                    puts ""
+                    menu
+                end
+            end
+            
+            puts "Thanks for visiting. I hope you're headed out to the bookstore!" #IS THIS BETTER UNDER LINE 24?
         end
 
         def menu
+            puts "Select an age range to see the best children's book suggestions!"
+            puts ""
             puts <<-MENU
-            "Select an age range to see the best children's book suggestions!"
             1. Ages 2-4
             2. Ages 5-7
             3. Ages 8-9
             4. Ages 10-12
             5. Surprise me!
+            Type "exit" to leave the program.
+            Type "menu" at any time to return to menu.
             MENU
+            puts ""
         end
 
-        def list_books
-            Book.all.each.with_index(1) do |book, index|
-                puts "#{index}. #{book.title}"
+        def print_preschoolbooks
+            ChildrensBooks::Book.preschoolbooks.each do |preschool_book|
+                puts ""
+                puts "Title: #{preschool_book.title}"
+                puts "Author: #{preschool_book.author}"
+                puts "Description: #{preschool_book.description}"
+                puts "Release Year: #{preschool_book.year}"
+                puts ""
             end
-        end
-
-        def list_books_by_age
-            if age == 2 || 3 || 4
-        end
-
-        def surprise_me
         end
     end
 end
+
+#puts ChildrensBooks::Book.print_preschoolbooks #.each do |preschool_book|
+                        #puts ""
+                        #puts "Title: #{preschool_book.title}"
+                        #puts "Author: #{preschool_book.author}"
+                        #puts "Description: #{preschool_book.description}"
+                        #puts "Release Year: #{preschool_book.year}"
+                        #puts ""
+                    #end
