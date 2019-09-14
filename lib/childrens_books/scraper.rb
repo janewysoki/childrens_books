4b55ab4637856ccb9678dfa70034e6c6fadee6ae
janewysoki <jane.wysoki@gmail.com>
2019-09-14 17:44:50 -0400

    Replace delete with gsub to fix missing letters

--- lib/childrens_books/scraper.rb
+++ lib/childrens_books/scraper.rb
@@ -1,9 +1,3 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative './book'
-
 module ChildrensBooks
     class Scraper
         def self.scrape
@@ -12,18 +6,19 @@ module ChildrensBooks
             
             doc.css(".views-row-odd").each do |node|
                 title = node.css("strong.field-content a").text
-                author = node.css("div.views-field-field-term-book-authors").text.strip.delete("By").split.join(" ")
+                #node.css("div.views-field-field-term-book-authors").text.gsub("By", "").strip
+                author = node.css("div.views-field-field-term-book-authors").text.gsub("By", "").strip
                 description = node.css("div.views-field-field-one-liner").text.strip
                 age = node.css("div.views-field-field-review-recommended-age").text.strip.delete("age").delete("+").split.join("")
                 year = node.css("div.views-field-field-canonical-date").text.strip.gsub(/[()]/, "") 
                 url = node.css("strong.field-content a").attr("href").value
                 ChildrensBooks::Book.new(title, author, description, age, year, url)
-                binding.pry
+                #binding.pry
             end
             
             doc.css(".views-row-even").each do |node|
                 title = node.css("strong.field-content a").text
-                author = node.css("div.views-field-field-term-book-authors").text.strip.delete("By").split.join(" ")
+                author = node.css("div.views-field-field-term-book-authors").text.gsub("By", "").strip
                 description = node.css("div.views-field-field-one-liner").text.strip
                 age = node.css("div.views-field-field-review-recommended-age").text.strip.delete("age").delete("+").split.join("")
                 year = node.css("div.views-field-field-canonical-date").text.strip.gsub(/[()]/, "")
@@ -33,6 +28,5 @@ module ChildrensBooks
         end
     end
 end
-
 #ChildrensBooks::Scraper.scrape
 
