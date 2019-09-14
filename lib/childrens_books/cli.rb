1133c5f22a9c075f97ff93ca275929f29fce501a
janewysoki <jane.wysoki@gmail.com>
2019-09-14 17:57:00 -0400

    Add colorize to improve readability

--- lib/childrens_books/cli.rb
+++ lib/childrens_books/cli.rb
@@ -1,50 +1,56 @@
+require 'colorize'
 module ChildrensBooks
     class CLI
 
         def call 
             ChildrensBooks::Scraper.scrape
             puts ""
-            puts "Welcome to the Children's Books Database!"
+            puts "Welcome to the Children's Books Database!".magenta
             puts ""
             @input = nil
             menu
             while @input != "exit" 
                 @input = gets.chomp
                 if @input == "1"
-                    puts "Here are some books 2-4 year olds will love:"
+                    puts "Here are some books 2-4 year olds will love:".green
                     books_array = ChildrensBooks::Book.preschoolbooks
                     print_books(books_array)
+                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "2"
-                    puts "Here are some books 5-7 year olds will love:"
+                    puts "Here are some books 5-7 year olds will love:".green
                     books_array = ChildrensBooks::Book.littlekidbooks
                     print_books(books_array)
+                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "3"
-                    puts "Here are some books 8-9 year olds will love:"
+                    puts "Here are some books 8-9 year olds will love:".green
                     books_array = ChildrensBooks::Book.bigkidbooks
                     print_books(books_array)
+                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "4"
-                    puts "Here are some books 10-12 year olds will love:"
+                    puts "Here are some books 10-12 year olds will love:".green
                     books_array = ChildrensBooks::Book.tweenbooks
                     print_books(books_array)
+                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "5"
-                    puts "Here's a randomly selected book for you to try:"
+                    puts "Here's a randomly selected book for you to try:".green
                     book_obj = ChildrensBooks::Book.all.sample
                     print_book(book_obj)
+                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "menu"
                     menu
                 elsif @input == "exit"
                     break
                 else 
-                    puts "Sorry, you did not select a valid number. Please try again."
+                    puts "Sorry, you did not select a valid number. Please try again.".red
                     puts ""
                     menu
                 end
             end
-            puts "Thanks for visiting. I hope you're headed out to the bookstore!" #IS THIS BETTER UNDER LINE 24?
+            puts "Thanks for visiting. I hope you're headed out to the bookstore!".magenta
         end
 
         def menu
-            puts "Select an age range to see the best children's book suggestions!"
+            puts "Select an age range to see the best children's book suggestions!".cyan
             puts ""
             puts <<-MENU
             1. Ages 2-4
