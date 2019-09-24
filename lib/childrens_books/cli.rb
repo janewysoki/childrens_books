75ad0d8bcbe66c4b399833b2de432c5dbef2a864
janewysoki <jane.wysoki@gmail.com>
2019-09-24 15:09:08 -0400

    Refactor code and add notes

--- lib/childrens_books/cli.rb
+++ lib/childrens_books/cli.rb
@@ -1,39 +1,30 @@
 module ChildrensBooks
     class CLI
-
+        @@ages = ["2-4", "5-7", "8-9", "10-12"]
         def call 
             ChildrensBooks::Scraper.scrape
+            @@preschoolbooks = ChildrensBooks::Book.preschoolbooks
+            @@littlekidbooks = ChildrensBooks::Book.littlekidbooks
+            @@bigkidbooks = ChildrensBooks::Book.bigkidbooks
+            @@tweenbooks = ChildrensBooks::Book.tweenbooks
+            @@books_by_age = [@@preschoolbooks, @@littlekidbooks, @@bigkidbooks, @@tweenbooks]
             puts ""
             puts "Welcome to the Children's Books Database!".magenta
             puts ""
-            @input = nil
+            @input = nil #has to have a value to use in while conditional
             menu
             while @input != "exit" 
-                @input = gets.chomp
-                if @input == "1"
-                    puts "Here are some books 2-4 year olds will love:".green
-                    books_array = ChildrensBooks::Book.preschoolbooks
-                    print_books(books_array)
-                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
-                elsif @input == "2"
-                    puts "Here are some books 5-7 year olds will love:".green
-                    books_array = ChildrensBooks::Book.littlekidbooks
-                    print_books(books_array)
-                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
-                elsif @input == "3"
-                    puts "Here are some books 8-9 year olds will love:".green
-                    books_array = ChildrensBooks::Book.bigkidbooks
-                    print_books(books_array)
-                    puts "Enter 'menu' for more options or 'exit' to leave the program.".green
-                elsif @input == "4"
-                    puts "Here are some books 10-12 year olds will love:".green
-                    books_array = ChildrensBooks::Book.tweenbooks
+                @input = gets.chomp #gets asks for user input/gets user input #input here comes in as string because of gets
+                if (1..4).include?(@input.to_i)
+                    index = @input.to_i - 1 #the index number of the element in the array #convert the input to an index
+                    puts "Here are some books #{@@ages[index]} year olds will love:".green
+                    books_array = @@books_by_age[index]
                     print_books(books_array)
                     puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "5"
                     puts "Here's a randomly selected book for you to try:".green
                     book_obj = ChildrensBooks::Book.all.sample
-                    print_book(book_obj)
+                    print_book(book_obj) #self here is an instance of the CLI class
                     puts "Enter 'menu' for more options or 'exit' to leave the program.".green
                 elsif @input == "menu"
                     menu
@@ -51,16 +42,12 @@ module ChildrensBooks
         def menu
             puts "Select an age range to see the best children's book suggestions!".cyan
             puts ""
-            puts <<-MENU
-            1. Ages 2-4
-            2. Ages 5-7
-            3. Ages 8-9
-            4. Ages 10-12
-            5. Surprise me!
-            Type "exit" to leave the program.
-            Type "menu" at any time to return to menu.
-            MENU
-            puts ""
+            @@ages.each.with_index(1) do |range, index| #index is reference point to location in array
+                puts "#{index}. Ages #{range}"
+            end
+            puts "5. Surprise me!"
+            puts "Type 'exit' to leave the program."
+            puts "Type 'menu' at any time to return to menu."
         end
 
         def print_books(array)
@@ -84,6 +71,8 @@ module ChildrensBooks
                 puts "Age: #{book.age}"
                 puts ""
         end
+    
+    
     end
 end
 
