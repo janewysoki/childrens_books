b3c043b1442c001c50eb17386c130d729894ba4c
janewysoki <jane.wysoki@gmail.com>
2019-09-24 21:01:39 -0400

    Correct class methods by removing to_a

--- lib/childrens_books/book.rb
+++ lib/childrens_books/book.rb
@@ -1,10 +1,10 @@
 module ChildrensBooks
     class Book
         attr_accessor :title, :author, :description, :age, :year, :url
-        @@all = []
+        @@all = [] 
 
         def initialize(title, author, description, age, year, url)
-            @title = title
+            @title = title 
             @author = author
             @description = description
             @age = age
@@ -19,25 +19,25 @@ module ChildrensBooks
 
         def self.preschoolbooks
             self.all.select do |book|
-                book if (2..4).to_a.include?(book.age.to_i)
+                book if (2..4).include?(book.age.to_i)
             end
         end
 
         def self.littlekidbooks
             self.all.select do |book|
-                book if (5..7).to_a.include?(book.age.to_i) 
+                book if (5..7).include?(book.age.to_i) 
             end
         end
 
         def self.bigkidbooks
             self.all.select do |book|
-                book if (8..9).to_a.include?(book.age.to_i) 
+                book if (8..9).include?(book.age.to_i) 
             end
         end
 
         def self.tweenbooks
             self.all.select do |book|
-                book if (10..12).to_a.include?(book.age.to_i) 
+                book if (10..12).include?(book.age.to_i) 
             end
         end
     end
